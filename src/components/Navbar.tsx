acfbe3842b380d3af6d876e6e52bb0fcfe57b3c3
KingOKarma <virtualdisaster782@gmail.com>
2021-05-06 18:28:44 +0100

    added access token finder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import '../css/Navbar.css';
 import { Navbar } from "react-bootstrap";
+// import { useLocation } from "react-router-dom";
 
 const Header = () => {
-   
+
+  let token = "Cannot find token, please try again"
+  let response = ""
+
+  const hash = window.location.hash.split("=")
+  if (!hash.includes("")) {
+    token = (hash[1].split("&")[0])
+    response = "Here is your access token:"
+  }
+  console.log(hash)
+
+
     return (
     <Navbar className="NavBar">
-  <Navbar.Brand> You should be able to see your Twitch access token</Navbar.Brand>
+  <Navbar.Brand>{response}</Navbar.Brand> 
   <Navbar.Collapse>
     <Navbar.Text>
-      Within the link in the search bar above
+      {token}
     </Navbar.Text>
   </Navbar.Collapse>
 </Navbar>
@@ -17,3 +29,9 @@ const Header = () => {
 }
 
     export default Header
+
+
+    // Notes of code for future referance
+  // const url_string = window.location.href
+  // const url = new URL(url_string);
+  // const access_token = url.searchParams.get("access_token");
