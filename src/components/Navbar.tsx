b4b5436135b12db89d86f907333bbd74b72186fa
KingOKarma <virtualdisaster782@gmail.com>
2021-05-15 22:36:27 +0100

    site now also tells you scopes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,13 +7,26 @@ const Header = () => {
 
   let token = "Cannot find token, please try again"
   let response = ""
+  let scopesResponse = ""
+  let newScopes: string[] = [];
 
   const hash = window.location.hash.split("=")
   if (!hash.includes("")) {
     token = (hash[1].split("&")[0])
     response = "Here is your access token:"
-  }
-  console.log(hash)
+    console.log(hash[1].split("&")[0])
+  
+  const tokenScopes = hash[2].split("&");
+  const scopes = tokenScopes[0].split("+");
+
+  scopes.forEach(element => {
+    const replace = new RegExp('%3A', 'g');
+    const newElement = element.replace(replace, ":")
+    newScopes.push(newElement)
+  });
+  scopesResponse = "Your Scopes are:";
+  console.log(newScopes.join(", "));
+}
 
 
     return (
@@ -21,7 +34,11 @@ const Header = () => {
   <Navbar.Brand>{response}</Navbar.Brand> 
   <Navbar.Collapse>
     <Navbar.Text>
-      {token}
+      {token} 
+      <br/ >
+      {scopesResponse}
+      <br/ >
+      {newScopes.join(" || ")}
     </Navbar.Text>
   </Navbar.Collapse>
 </Navbar>
